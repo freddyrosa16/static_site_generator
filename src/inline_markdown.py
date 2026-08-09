744ac5c9bbb56bc6742ef7ddace312c65248bd4c
Freddy Rosa <freddy.rosa16@gmail.com>
2026-08-08 23:07:22 -0400

    Add extract_markdown_images and extract_markdown_links function stubs

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
@@ -17,3 +18,7 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
                 else:
                     new_nodes.append(TextNode(parts[i], text_type))
     return new_nodes
+
+def extract_markdown_images(text):
+
+def extract_markdown_links(text):
