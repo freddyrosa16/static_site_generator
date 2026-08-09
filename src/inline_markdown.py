1f2b7408bf8eafa3b12b35b3e8b9128e00ffdc6c
Freddy Rosa <freddy.rosa16@gmail.com>
2026-08-09 11:25:08 -0400

    Add tests for extract_markdown_images and extract_markdown_links functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -20,5 +20,9 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
     return new_nodes
 
 def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
 
 def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
