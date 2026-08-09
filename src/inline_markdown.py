4755eb722228893995b43fcf643f9f592a4f9544
Freddy Rosa <freddy.rosa16@gmail.com>
2026-08-08 20:41:00 -0400

    Add split_nodes_delimiter function and corresponding tests for TextNode handling

---
+++ src/inline_markdown.py
@@ -0,0 +1,19 @@
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+        else:
+            parts = old_node.text.split(delimiter)
+            if len(parts) % 2 == 0:
+                raise Exception("Please finish your MD because there is a delimiter missing")
+            for i in range(len(parts)):
+                if parts[i] == "":
+                    continue
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(parts[i], TextType.TEXT))
+                else:
+                    new_nodes.append(TextNode(parts[i], text_type))
+    return new_nodes
--- src/splitnode.py
+++
@@ -1,3 +0,0 @@
-from textnode import TextNode
-
-def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
